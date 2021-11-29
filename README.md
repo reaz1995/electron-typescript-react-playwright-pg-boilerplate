09f3eff92f4b983ea0645ad83fb6363060a108a0
Sebastian <sebasitan.langner00@gmail.com>
2021-11-29 03:27:35 +0100

    add: playwright presentation

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Ready to go boilerplate of electron, typescript, react, mui, pg, playwright
 
 Works on wsl2/Windows, postgres need some dependencies on wsl2, and on Windows you need to install postgres and set PATH, wsl2 need also to set up for Xdisplay to get GUI, more info soon ....
+
+To exit opened App use shortcut:
+`Ctrl + W`
--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,7 +1,55 @@
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, Button, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import theme from "../theme";
 import Greetings from "./Greetings";
+import { chromium } from "playwright";
+
+async function initiaizeBrowser(): Promise<void> {
+  const browser = await chromium.launch({
+    // executablePath:
+    //   "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+    channel: "chrome",
+    headless: false,
+  });
+
+  const browserContext = await browser.newContext({ viewport: null });
+
+  // ovveride browser
+  const enabledEvasions = [
+    "chrome.app",
+    "chrome.csi",
+    "chrome.loadTimes",
+    "navigator.webdriver",
+    "sourceurl",
+    "webgl.vendor",
+    "window.outerdimensions",
+  ];
+
+  const evasions = enabledEvasions.map((e) =>
+    require(`puppeteer-extra-plugin-stealth/evasions/${e}/index.js`)
+  );
+  const stealth = {
+    callbacks: [],
+    async evaluateOnNewDocument(...args) {
+      this.callbacks.push({ cb: args[0], a: args[1] });
+    },
+  };
+  evasions.forEach((e) => {
+    e().onPageCreated(stealth);
+  });
+  for (const evasion of stealth.callbacks) {
+    await browserContext.addInitScript(evasion.cb, evasion.a);
+  }
+
+  const page0 = await browserContext.newPage();
+  await page0.goto("https://bot.sannysoft.com/");
+  const page1 = await browserContext.newPage();
+  await page1.goto("https://pixelscan.net/");
+  const page2 = await browserContext.newPage();
+  await page2.goto("https://abrahamjuliot.github.io/creepjs/");
+  const page3 = await browserContext.newPage();
+  await page3.goto("https://bot.incolumitas.com/");
+}
 
 export default function App(): JSX.Element {
   return (
@@ -14,9 +62,18 @@ export default function App(): JSX.Element {
           backgroundColor: (theme) => theme.palette.background.default,
         }}
       >
-        <main>
+        <main
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           {/* This is where your app content should go */}
           <Greetings />
+          <Button variant="contained"  onClick={initiaizeBrowser}>
+            Open playwright Chrome Browser
+          </Button>
         </main>
       </Box>
     </ThemeProvider>
